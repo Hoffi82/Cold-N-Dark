44826b49dda213c75b2cf914ddda1ef73b6db466
Hoffi82 <5yggzvn2rs@privaterelay.appleid.com>
2026-08-18 14:35:56 +0200

    Offline-Service-Worker für die Cold N Dark App hinzufügen

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE = 'cold-n-dark-v1';
+const CORE = [
+  './app.html',
+  './mehr-app.html',
+  './krieg-app.html',
+  './cwl-app.html',
+  './mitglieder-app.html',
+  './news.html',
+  './regeln.html',
+  './Wappen.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(cached => cached || caches.match('./app.html')))
+  );
+});
