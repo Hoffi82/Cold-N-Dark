79d25741e35c6f9b5df94de978c83c4e73862793
Hoffi82 <5yggzvn2rs@privaterelay.appleid.com>
2026-08-31 21:33:15 +0200

    TYBMC: ersten CK mit Ergebnis COC Clan eintragen

---
+++ tybmc-ck-daten.js
@@ -0,0 +1,12 @@
+// TYBMC CK-Daten
+window.TYBMC_CK_HISTORY = [
+  {
+    opponent: 'COC Clan',
+    size: '25 vs. 25',
+    tybmcStars: 75,
+    opponentStars: 74,
+    tybmcDestruction: 100,
+    opponentDestruction: 98.40,
+    result: 'Sieg'
+  }
+];
